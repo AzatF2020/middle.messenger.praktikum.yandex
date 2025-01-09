16c129e8f4e402fec052694f0317acb11906c5ff
Азат <azat@MacBook-Pro-Azat.local>
2025-01-09 22:13:15 +0300

    Поправил EOF

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 import path from 'node:path';
-import fs from 'node:fs';
 import pageData from './pages-data';
 
 export default defineConfig({
     plugins: [
         handlebars({
-            context(pagePath) {
+            context(pagePath: string | number) {
                 return pageData[pagePath];
             },
             partialDirectory: path.resolve(__dirname, `src/modules`),
@@ -18,24 +17,25 @@ export default defineConfig({
         host: true,
     },
     build: {
+        emptyOutDir: true,
         rollupOptions: {
             output: {
                 dir: path.resolve(__dirname, 'dist'),
             },
             input: [
-                'chat-popups.html',
-                'chat-search.html',
-                'chat.html',
-                'chats.html',
-                'index.html',
-                'login.html',
-                'modal.html',
-                'not-found.html',
-                'profile-change-data.html',
-                'profile-new-password.html',
-                'profile.html',
-                'register.html',
-                'server-error.html'
+                './src/pages/chat-popups.html',
+                './src/pages/chat-search.html',
+                './src/pages/chat.html',
+                './src/pages/chats.html',
+                './src/pages/index.html',
+                './src/pages/login.html',
+                './src/pages/modal.html',
+                './src/pages/not-found.html',
+                './src/pages/profile-change-data.html',
+                './src/pages/profile-new-password.html',
+                './src/pages/profile.html',
+                './src/pages/register.html',
+                './src/pages/server-error.html'
             ]
         }
     },
